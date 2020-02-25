54bf5636eb09a4526994008d5eb1c72d67be65aa
Cool Lin <andrzejkom2016@gmail.com>
2020-02-25 12:50:38 +0100

    class ShipsLocation incorporated

---
+++ ShipsLocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noweBShip 
+{
+    
+    public class ShipsLocation
+     {
+        public List<Ship> Ships { get; set; }
+
+        public ShipsLocation()
+        {
+            Ships = new List<Ship>();
+            Ship aircar = new Carrier();
+            Ships.Add(aircar);
+            Ship batship = new Battleship();
+            Ships.Add(batship);
+            Ship cruis = new Cruiser();
+            Ships.Add(cruis);
+            Ship submar = new Submarine();
+            Ships.Add(submar);
+            Ship destr = new Destroyer();
+            Ships.Add(destr);
+        }
+     }
+}
