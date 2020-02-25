3a188135c506d148fa4cecf2f3cc9abae02eced2
Cool Lin <andrzejkom2016@gmail.com>
2020-02-25 14:50:34 +0100

    game update

--- Game.cs
+++ Game.cs
@@ -9,8 +9,22 @@ namespace noweBShip
         private Random random = new Random();
         public void StartGame()
         {
+            Console.WriteLine("Player vs. AI battleship game");
             Ocean EnOcean = new Ocean();
 		    Ocean MyOcean = new Ocean();
+            ShipsLocation EnLocation = new ShipsLocation();
+            ShipsLocation MyLocation = new ShipsLocation();
+            // placement enemy ships in Ships (List)
+            foreach (Ship item in EnLocation.Ships)
+            {
+                string type = item.Name;
+                if (type == "CARRIER")
+                {
+                    item.Cover[0] = ""
+                } 
+            }
 		}
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace noweBShip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Battleship!");
+            Game game = new Game();
+            game.StartGame();
         }
     }
 }
