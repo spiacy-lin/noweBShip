ce2e512c0316ffe09b67bc8f85545ac6ad27ecb1
Cool Lin <andrzejkom2016@gmail.com>
2020-02-25 12:46:55 +0100

    class Ocean incorporated

---
+++ Ocean.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipsGameCC 
+{
+    class Ocean 
+	{
+    	
+		private Square[,] Board = new Square[10,10];
+		public Ocean()
+		{
+			for(int x = 0; x<10; x++)
+			{
+				for(int y = 0; y<10; y++)
+				{
+					Board[x,y] = new Square();
+				}
+			}
+		}
+	}
+}
