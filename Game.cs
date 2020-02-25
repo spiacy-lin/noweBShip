45c2c5aa30561cf8b77443fe57a147dc4dfb2406
Cool Lin <andrzejkom2016@gmail.com>
2020-02-25 12:54:08 +0100

    class game incorporated

---
+++ Game.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noweBShip
+{
+    class Game
+    {
+        private Random random = new Random();
+        public void StartGame()
+        {
+            Ocean EnOcean = new Ocean();
+		    Ocean MyOcean = new Ocean();
+		}
+    }
+}
