ea722a1fc2e7c17da1156fd7928da5533c7eedf3
Cool Lin <andrzejkom2016@gmail.com>
2020-02-25 12:22:07 +0100

    initial stage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace noweBShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Battleship!");
+        }
+    }
+}
